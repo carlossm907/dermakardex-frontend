5f1b026c11de8f4533be258517790f16dd697d27
Carlos <U202015274@upc.edu.pe>
2026-02-05 23:48:16 -0500

    refactor: format code for consistency in iam.api.ts

--- src/modules/iam/infrastructure/api/iam.api.ts
+++ src/modules/iam/infrastructure/api/iam.api.ts
@@ -1,24 +1,24 @@
-import apiClient from '@shared/config/api.config'
-import type { UserRole } from '../../domain/models/user.model'
+import apiClient from "@/shared/config/api.config";
+import type { UserRole } from "../../domain/models/user.model";
 
 export interface SignInRequest {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export interface SignUpRequest {
-    username: string;
-    password: string;
-    role: UserRole;
-    fullName: string;
+  username: string;
+  password: string;
+  role: UserRole;
+  fullName: string;
 }
 
 export interface AuthenticatedUserResponse {
-    id: number;
-    fullName: string;
-    username: string;
-    roles: UserRole[];
-    token: string;
+  id: number;
+  fullName: string;
+  username: string;
+  roles: UserRole[];
+  token: string;
 }
 
 export interface UserResponse {
@@ -29,25 +29,26 @@ export interface UserResponse {
 }
 
 export const iamApi = {
-    signIn: async (data: SignInRequest): Promise<AuthenticatedUserResponse> => {
-        const response = await apiClient.post<AuthenticatedUserResponse>('/authentication/sign-in' , data);
-    
-    return response.data
+  signIn: async (data: SignInRequest): Promise<AuthenticatedUserResponse> => {
+    const response = await apiClient.post<AuthenticatedUserResponse>(
+      "/authentication/sign-in",
+      data,
+    );
 
-    },
+    return response.data;
+  },
 
-    signUp: async (data: SignUpRequest): Promise<void> => {
-        await apiClient.post('/authentication/sign-up', data)
-    },
+  signUp: async (data: SignUpRequest): Promise<void> => {
+    await apiClient.post("/authentication/sign-up", data);
+  },
 
-    getUserById: async (id :number): Promise<UserResponse> => {
-        const response = await apiClient.get<UserResponse>(`/users/${id}`);
-        return response.data;
-    },
+  getUserById: async (id: number): Promise<UserResponse> => {
+    const response = await apiClient.get<UserResponse>(`/users/${id}`);
+    return response.data;
+  },
 
-    getAllUsers: async(): Promise<UserResponse[]> => {
-        const response = await apiClient.get<UserResponse[]>(`/users`);
-        return response.data;
-    }
-
-}
+  getAllUsers: async (): Promise<UserResponse[]> => {
+    const response = await apiClient.get<UserResponse[]>(`/users`);
+    return response.data;
+  },
+};
