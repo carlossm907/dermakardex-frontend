53c1e93650f8538c00d264f7a20c910e8592a0e0
Carlos <U202015274@upc.edu.pe>
2026-02-17 17:29:32 -0500

    feat: add Sale model and related interfaces for sales management

---
+++ src/modules/sales/domain/models/sale.model.ts
@@ -0,0 +1,34 @@
+import type { CreateSaleItemData, SaleItem } from "./sale-item.model";
+import type { CreateSalePaymentData, SalePayment } from "./sale-payment.model";
+import type { SaleStatus } from "./sale-status.model";
+
+export interface Sale {
+  id: number;
+  ticketNumber: string;
+  customerDni: string;
+  customerFullName: string;
+  saleDate: string;
+  saleTime: string;
+  observation: string;
+  total: number;
+  status: SaleStatus;
+  items: SaleItem[];
+  payments: SalePayment;
+}
+
+export interface CreateSaleData {
+  customerDni: string;
+  observation: string;
+  items: CreateSaleItemData[];
+  payments: CreateSalePaymentData;
+}
+
+export interface SaleListItem {
+  id: number;
+  ticketNumber: string;
+  customerFullName: string;
+  saleDate: string;
+  saleTime: string;
+  total: number;
+  status: SaleStatus;
+}
